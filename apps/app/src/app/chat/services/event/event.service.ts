08b8992b26d8b5c26f190858669f19ff75fae2b2
Daniel Kreitsch <hello@danielkreitsch.com>
2024-07-15 00:31:35 +0200

    chore(app): setup environment variables

--- apps/app/src/app/chat/services/event/event.service.ts
+++ apps/app/src/app/chat/services/event/event.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Festival } from '../../models/festival';
+import {environment} from "../../../../environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
-
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient) {
   }
 
   getFestivals(searchText: Festival): Observable<Festival> {
-    return this.http.post<Festival>('https://festivalgpt-backend.s1.glowdragon.de/api/chat/message', searchText)
+    return this.http.post<Festival>(`${environment.apiUrl}/chat/message`, searchText)
   }
 }
---
+++ apps/app/src/environment.template.ts
@@ -0,0 +1,4 @@
+const env: any = {}
+export const environment = {
+  apiUrl: env["API_URL"] ?? "http://localhost:8080/api",
+}
